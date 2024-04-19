793b495a4bf165e88463136db3788c6b54caaf03
mmi-ku <161840539+mmi-ku@users.noreply.github.com>
2024-04-19 14:49:03 +0000

    課題2editページ作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books'=>'books#create'
   
-  get 'books/index'
-  get 'books/show'
+  # get 'books/index'
+  get 'books' => 'books#index'
+  
+  
+  get 'books/:id' => 'books#show',as: 'book'
+  
   get 'books/edit'
-  get 'homes/top'
+  # get 'homes/top'
+  get 'top' => 'homes#top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
