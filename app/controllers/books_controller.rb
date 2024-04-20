7e2352e1a5e821a729a5220c9b5937b4212fc542
mmi-ku <161840539+mmi-ku@users.noreply.github.com>
2024-04-20 13:33:57 +0000

    フラッシュメッセージ表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,18 @@
 class BooksController < ApplicationController
-  
-  def new
-    
-    @book=Book.new
-  end
-  
+
   def create
     book=Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+    else
+      flash.now[:notice] = "Book was failed to created."
+      render :new
+    end
   end
 
   def index
     @books=Book.all
+    # @book=Book.new
   end
 
   def show
@@ -22,16 +22,26 @@ class BooksController < ApplicationController
   def edit
     @book= Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
-  
+
+  def destroy
+    @book= Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
   get 'books/new'
   post 'books'=>'books#create'
-
-  # get 'books/index'
   get 'books' => 'books#index'
-
-
   get 'books/:id' => 'books#show', as: 'book'
-
-  get 'books/:id' => 'books#edit', as: 'edit_book'
-
-  patch 'book/:id' => 'book#update', as: 'update_book'
-
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   get 'top' => 'homes#top'
+  delete 'books/:id' => 'books#destroy', as:'destroy_book'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
