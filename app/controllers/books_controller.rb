736bb50d92ff0bdeb9b1c5ada0fb5310618de16e
mmi-ku <161840539+mmi-ku@users.noreply.github.com>
2024-04-20 01:13:08 +0000

    課題2updateまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,11 +16,20 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book=Book.find(params[:id])
+    @book= Book.find(params[:id])
   end
 
   def edit
+    @book= Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  private
   
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   get 'books/new'
   post 'books'=>'books#create'
-  
+
   # get 'books/index'
   get 'books' => 'books#index'
-  
-  
-  get 'books/:id' => 'books#show',as: 'book'
-  
-  get 'books/edit'
-  # get 'homes/top'
+
+
+  get 'books/:id' => 'books#show', as: 'book'
+
+  get 'books/:id' => 'books#edit', as: 'edit_book'
+
+  patch 'book/:id' => 'book#update', as: 'update_book'
+
   get 'top' => 'homes#top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
